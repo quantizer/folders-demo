0600f867a981efb945236c9b84a9b04b247afabb
Ivan Diadin <jlamep@gmail.com>
2018-03-28 06:15:26 +0300

    Added Bootstrap 4 styles.

--- assets/js/components/CreateFolder.js
+++ assets/js/components/CreateFolder.js
@@ -5,11 +5,14 @@ class CreateFolder extends React.Component {
         const {onSubmitForm, onChangeInput, value, parent } = this.props;
 
         return (
-            <div>
-                Add to folder: { parent ? parent.name : `root` }
+            <div className="col-lg-3 col-sm-12 col-md-6">
+                <h4>Add to folder: { parent ? parent.name : `root` }</h4>
                 <form onSubmit={onSubmitForm}>
-                    <input placeholder={'Folder name'} type="text" value={value} onChange={onChangeInput}/>
-                    <button>Create folder</button>
+                    <div className="form-group">
+                        <label htmlFor="folder_name">Folder name</label>
+                        <input className="form-control" name="folder_name" placeholder={'Enter folder name'} type="text" value={value} onChange={onChangeInput}/>
+                    </div>
+                    <button className="btn btn-primary">Create folder</button>
                 </form>
             </div>
         )
--- assets/js/components/FolderItem.js
+++ assets/js/components/FolderItem.js
@@ -9,9 +9,10 @@ class FolderItem extends React.Component {
             <li key={folder.id}>
                 <div>
                     { folder.name }
+                    <button type="button" className="btn btn-link btn-sm" onClick={(e) => onItemClick(folder, e)}>Select</button>
                     { folder.child_folders ? <FolderList onItemClick={onItemClick} folders={folder.child_folders} /> : `` }
                 </div>
-                <button onClick={(e) => onItemClick(folder, e)}>Select</button>
+
             </li>
         )
     }
